352841cb81b2f4aab82e9f0036f5e231bdb96c69
xushaolong <2573953939@qq.com>
2023-05-06 17:16:41 +0800

    func 1、设置时区

--- finance/src/main/java/cn/codegraffiti/alone/finance/controller/ReTestController.java
+++ finance/src/main/java/cn/codegraffiti/alone/finance/controller/ReTestController.java
@@ -1,16 +1,19 @@
 package cn.codegraffiti.alone.finance.controller;
 
 import cn.codegraffiti.alone.core.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/re_test")
 public class ReTestController {
 
     @GetMapping(value = "/json_serial")
     public R<String> jsonSerial() {
+        log.info("currentTimeMillis:{}", System.currentTimeMillis());
         return R.ok("re_test");
     }
 
