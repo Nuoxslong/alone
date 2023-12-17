cc32365683720fae3a61d6f579fdcd4539b95c61
xslong <2573953939@qq.com>
2023-12-17 17:20:04 +0800

    feat:添加工具类

--- component/security/src/main/java/cn/codegraffiti/alone/security/endpoint/AuthEndpoint.java
+++ component/security/src/main/java/cn/codegraffiti/alone/security/endpoint/AuthEndpoint.java
@@ -1,6 +1,7 @@
 package cn.codegraffiti.alone.security.endpoint;
 
 import cn.codegraffiti.alone.common.R;
+import cn.codegraffiti.alone.common.utils.HttpUtil;
 import cn.codegraffiti.alone.common.utils.JsonUtil;
 import cn.codegraffiti.alone.security.domain.AloneOauthToken;
 import cn.codegraffiti.alone.security.properties.ComponentSecurityProperties;
@@ -17,25 +18,25 @@ public class AuthEndpoint {
 
     final ComponentSecurityProperties properties;
 
-    // @GetMapping(value = "/access_token")
-    // public R<AloneOauthToken> callback(@RequestParam String code) {
-    //     String authDomain = properties.getAuthDomain();
-    //     String intranetDomain = this.properties.getIntranetDomain();
-    //     WebClient
-    //     Http request = HttpUtil.createPost(authDomain + "/oauth2/token");
-    //     request.basicAuth(properties.getClientId(), properties.getClientSecret());
-    //     request.form(Map.of("code", code, "grant_type", "authorization_code", "redirect_uri", intranetDomain + "/oauth2/access_token"));
-    //     HttpResponse response = request.execute();
-    //     return R.ok(JsonUtil.toBean(response.body(), AloneOauthToken.class));
-    // }
-    //
-    // @GetMapping(value = "/refresh_token")
-    // public R<AloneOauthToken> refreshToken(@RequestParam String token) {
-    //     String authDomain = properties.getAuthDomain();
-    //     HttpRequest request = HttpUtil.createPost(authDomain + "/oauth2/token");
-    //     request.basicAuth(properties.getClientId(), properties.getClientSecret());
-    //     request.form(Map.of("refresh_token", token, "grant_type", "refresh_token"));
-    //     HttpResponse response = request.execute();
-    //     return R.ok(JsonUtil.toBean(response.body(), AloneOauthToken.class));
-    // }
+    @GetMapping(value = "/access_token")
+    public R<AloneOauthToken> callback(@RequestParam String code) {
+        String authDomain = properties.getAuthDomain();
+        String intranetDomain = this.properties.getIntranetDomain();
+        String responseBody = HttpUtil.post_form(
+                authDomain + "/oauth2/token",
+                Map.of("code", code,
+                        "grant_type", "authorization_code",
+                        "redirect_uri", intranetDomain + "/oauth2/access_token"),
+                HttpUtil.basicAuth(properties.getClientId(), properties.getClientSecret()));
+        return R.ok(JsonUtil.toBean(responseBody, AloneOauthToken.class));
+    }
+
+    @GetMapping(value = "/refresh_token")
+    public R<AloneOauthToken> refreshToken(@RequestParam String token) {
+        String authDomain = properties.getAuthDomain();
+        String responseBody = HttpUtil.post_form(authDomain + "/oauth2/token",
+                Map.of("refresh_token", token, "grant_type", "refresh_token"),
+                HttpUtil.basicAuth(properties.getClientId(), properties.getClientSecret()));
+        return R.ok(JsonUtil.toBean(responseBody, AloneOauthToken.class));
+    }
 }
