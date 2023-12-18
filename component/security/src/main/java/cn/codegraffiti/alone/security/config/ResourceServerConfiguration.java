f5d3ea3fd016b1d053c1bb2ee170a0307859cab3
xslong <2573953939@qq.com>
2023-12-18 09:53:17 +0800

    feat:更新 spring-authorization-server 版本

--- component/security/src/main/java/cn/codegraffiti/alone/security/config/ResourceServerConfiguration.java
+++ component/security/src/main/java/cn/codegraffiti/alone/security/config/ResourceServerConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,21 +16,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity // 开启鉴权服务
 public class ResourceServerConfiguration {
 
-    // @Bean
-    // public SecurityFilterChain httpSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    //     httpSecurity.csrf(Customizer.withDefaults());
-    //     // 所有请求都进行拦截
-    //     httpSecurity.authorizeHttpRequests()
-    //             .requestMatchers("/re_test/**", "/error", "/favicon.ico").permitAll()
-    //             .anyRequest().authenticated();
-    //     // 关闭session
-    //     httpSecurity.sessionManagement().disable();
-    //     // 配置资源服务器的无权限，无认证拦截器等 以及JWT验证
-    //     httpSecurity.oauth2ResourceServer()
-    //             .accessDeniedHandler(new AloneAccessDeniedHandler())
-    //             .authenticationEntryPoint(new AloneAuthenticationEntryPoint())
-    //             .jwt();
-    //     return httpSecurity.build();
-    // }
+    @Bean
+    public SecurityFilterChain httpSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        // 所有请求都进行拦截
+        httpSecurity.authorizeHttpRequests(http ->
+                http.requestMatchers("/re_test/**", "/error", "/favicon.ico", "/access_token").permitAll()
+                        .anyRequest().authenticated());
+        // 关闭session
+        httpSecurity.sessionManagement(Customizer.withDefaults());
+        // 配置资源服务器的无权限，无认证拦截器等 以及JWT验证
+        httpSecurity.oauth2ResourceServer(authorizeRequests ->
+                authorizeRequests
+                        .accessDeniedHandler(new AloneAccessDeniedHandler())
+                        .authenticationEntryPoint(new AloneAuthenticationEntryPoint())
+                        .jwt(Customizer.withDefaults()));
+        return httpSecurity.build();
+    }
 
 }
